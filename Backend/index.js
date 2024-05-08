4669037aa68f014861274288d21ed6bbcba045b9
Asif <heyasif@asif.local>
2024-05-08 23:56:40 +0530

    backend

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const cors = require("cors");
+const { ConnectDatabase } = require('./config/db');
+const { userRouter } = require("./routes/user.routes");
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", userRouter);
+
+app.get("/", (req, res) => {
+    try {
+      res.status(200).send({ msg: "Server is Runnning" });
+    } catch (error) {
+      console.log("Error", error);
+    }
+  });
+
+app.listen(process.env.PORT, () => {
+  ConnectDatabase();
+  console.log(`Connected Server to Port ${process.env.PORT}`);
+});
